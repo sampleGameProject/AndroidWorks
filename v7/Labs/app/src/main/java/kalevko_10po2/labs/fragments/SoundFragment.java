69708f551c455a588a4855fc5bb11187bd57027d
admin <kalevko.gp@gmail.com>
2014-11-16 23:39:57 +0300

    5

--- v7/Labs/app/src/main/java/kalevko_10po2/labs/fragments/SoundFragment.java
+++ v7/Labs/app/src/main/java/kalevko_10po2/labs/fragments/SoundFragment.java
@@ -1,16 +1,22 @@
 package kalevko_10po2.labs.fragments;
 
+import android.content.Context;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Vibrator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import kalevko_10po2.R;
 
 
-public class SoundFragment extends Fragment {
+public class SoundFragment extends Fragment implements View.OnClickListener{
 
+    Button button;
+    Vibrator v;
 
     public SoundFragment() {
         // Required empty public constructor
@@ -20,7 +26,43 @@ public class SoundFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sound, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_sound, container, false);
+
+        button = (Button) view.findViewById(R.id.buttonPlay);
+        button.setOnClickListener(this);
+
+        v = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+
+        return view;
     }
 
+    MediaPlayer mediaPlayer;
+
+    @Override
+    public void onClick(View view) {
+
+        v.vibrate(500);
+
+        if(mediaPlayer == null){
+            mediaPlayer = MediaPlayer.create(getActivity(), R.raw.pobediteley_ne_sudyat);
+            mediaPlayer.start();
+            button.setText("Stop");
+        }
+        else{
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+            button.setText("Play");
+        }
+
+    }
+
+    @Override
+    public void onDestroy(){
+        if(mediaPlayer != null){
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        }
+    }
 }
